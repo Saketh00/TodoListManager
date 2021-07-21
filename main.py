1d664b35de681db025e602641e668af3f86855aa
Saketh00 <sakethch310@gmail.com>
2021-07-21 12:45:54 +0530

    Modifies main and flask run

--- main.py
+++ main.py
@@ -1,42 +1,24 @@
 import psycopg2
 import sys
+from flask import Flask, render_template
 
+app=Flask("Todolist site")
+
+dbconn=psycopg2.connect("dbname=todolist")
+
+@app.route("/")
+def index():
+    cursor=dbconn.cursor()
+    cursor.execute("select count(*) from list")
+    ntasks=cursor.fetchone()[0]
+    return render_template("main.html",ntasks=ntasks)
+
+@app.route("/tasks")
 def task_list():
-    dbconn=psycopg2.connect("dbname=todolist")
     cursor=dbconn.cursor()
     cursor.execute("select * from list")
     tasks=cursor.fetchall()
-    for id,task in tasks:
-        print(id, task)
-
-
-def Insert():
-    dbconn=psycopg2.connect("dbname=todolist")
-    cursor=dbconn.cursor()
-    f=open("insert.sql")
-    sql_code=f.read()
-    f.close()
-    cursor.execute(sql_code)
-    dbconn.commit()
-
-def Create():
-    dbconn=psycopg2.connect("dbname=todolist")
-    cursor=dbconn.cursor()
-    f=open("list.sql")
-    sql_code=f.read()
-    f.close()
-    cursor.execute(sql_code)
-    dbconn.commit()
-
-def main(arg):
-    if arg == "Create":
-        Create()
-    elif arg=="Insert":
-        Insert()
-    elif arg=="ListTask":
-        task_list()
-    else:
-        print(f"Unknown command {arg}")
+    return render_template("tasklist.html", tasks=tasks)
 
 if __name__=="__main__":
-    main(sys.argv[1])
+    app.run()
